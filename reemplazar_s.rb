66072d1d8375fb26bc3f1c6678de43b5ae4ec046
Alvaro Hilario <king314@gmail.com>
2013-08-13 13:34:41 -0700

    Create reemplazar_s.rb

---
+++ reemplazar_s.rb
@@ -0,0 +1,15 @@
+=begin
+Probado reemplazar las s digitadas por th
+Y pensado en hacer algunas mejores
+=end
+
+print "Como se llama su nombre suyo de usted?"
+user_input = gets.chomp
+user_input.downcase!
+
+if user_input.include? "s"
+    my_string = user_input.gsub!(/s/, "th")
+    puts "El valor real es: #{my_string}!"
+else
+    print "Usted no tiene s"
+end
