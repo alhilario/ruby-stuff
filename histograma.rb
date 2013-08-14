d16ddefa09d6fa72474c2ba5563f291ffdba79ef
Alvaro Hilario <king314@gmail.com>
2013-08-14 08:48:28 -0700

    Create histograma.rb

---
+++ histograma.rb
@@ -0,0 +1,10 @@
+puts "Digite un texto: "
+text = gets.chomp
+
+words = text.split(" ")
+frequencies = Hash.new(0)
+words.each do |word| frequencies[word] +=1 end
+frequencies = frequencies.sort_by {|p, m| m}
+frequencies.reverse!
+
+frequencies.each do |word, frequency| puts word + " " + frequency.to_s end
