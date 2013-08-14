a5cb83bbf53358748cc727cee44fb21f153492fa
Alvaro Hilario <king314@gmail.com>
2013-08-13 23:04:02 -0700

    Create busca_palabra.rb

---
+++ busca_palabra.rb
@@ -0,0 +1,14 @@
+puts "Digite un parrafo: "
+text = gets.chomp
+puts "Digite una palabra a buscar: "
+redact = gets.chomp
+
+words = text.split(" ")
+
+words.each do |word|
+    if word != redact
+        print word + " "
+    else
+        print "REDACTED " 
+    end
+end
